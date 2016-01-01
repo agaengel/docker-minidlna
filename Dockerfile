a8ee141255ea68cfc3fb2e61d0b9a22baebd7c8e
Alexander Gängel <alexander@gaengel.de>
2016-01-01 10:57:52 +0100

    save work

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,21 @@
-FROM debian:wheezy
+FROM debian:jessie
 
-RUN apt-get update && apt-get upgrade && apt-get install -y minidlna
+RUN awk '$1 ~ "^deb" { $3 = $3 "-backports"; print; exit }' /etc/apt/sources.list > /etc/apt/sources.list.d/backports.list
+
+RUN apt-get update && \
+    apt-get -t jessie-backports install -y \
+	minidlna && \
+    apt-get install -y \
+	supervisor && \  
+    apt-get clean && \
+    rm -rf /var/lib/apt/lists/*
+
 ADD ./run.sh /run.sh
 
-ENTRYPOINT ["/run.sh"]
+# add supervisor conf file for app
+ADD supervisor_minidlna.conf /etc/supervisor/conf.d/minidlna.conf
+
+EXPOSE 1900 8200
+
+#ENTRYPOINT ["/run.sh"]
+ENTRYPOINT ["/bin/bash"]
--- run.sh
+++ run.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-set -e
+#set -e
 
-> /etc/minidlna.conf
+# > /etc/minidlna.conf
 
 for VAR in `env`; do
     if [[ $VAR =~ ^MINIDLNA_ ]]; then
         minidlna_name=`echo "$VAR" | sed -r "s/MINIDLNA_(.*)=.*/\1/g" | tr '[:upper:]' '[:lower:]'`
         minidlna_value=`echo "$VAR" | sed -r "s/.*=(.*)/\1/g"`
-        echo "${minidlna_name}=${minidlna_value}" >> /etc/minidlna.conf
+	echo ${minidlna_name}
+	sed -i -e "s/$minidlna_name=.*/$minidlna_name=$minidlna_value/g" /etc/minidlna.conf
+	echo sed -i -e "s/$minidlna_name=.*/$minidlna_name=$minidlna_value/g" etc/minidlna.conf
+        #echo "${minidlna_name}=${minidlna_value}" >> /etc/minidlna.conf
     fi
 done
 
-exec /usr/bin/minidlna -d $@
+#exec bash
